3fd024f0071cced4260338fc212f5e2b8e2c2e67
Matthew Carter <phuded@gmail.com>
2013-08-24 15:37:12 +0100

    First attempt at comparing similar hands

--- src/main/poker/util/RoundWinnerDetector.groovy
+++ src/main/poker/util/RoundWinnerDetector.groovy
@@ -1,7 +1,7 @@
 package main.poker.util
 
+import main.poker.card.Card
 import main.poker.player.Player
-import main.poker.hand.Hand
 import main.poker.hand.HandType
 
 /**
@@ -30,7 +30,70 @@ class RoundWinnerDetector {
         }
         else{
             //Complex LOGIC - TO DO
+            getBetterHand(winningPlayers)
         }
 
     }
+
+
+    static getBetterHand(List<Player> winningPlayers){
+        println "MAIN: Winner: " + isBetterHand(winningPlayers[0],winningPlayers[1])
+
+    }
+
+
+    static isBetterHand(Player playerA, Player playerB){
+
+        int numberOfCards = playerA.bestHand.cards.size()
+
+        int cardComparison = compareCards(playerA.bestHand.cards,playerB.bestHand.cards)
+
+        if(cardComparison == 1){
+            return playerA
+        }
+        else if(cardComparison == 2){
+            return playerB
+        }
+        else{
+            if(numberOfCards < 5){
+                int secondaryCardComparison = compareCards(playerA.bestHand.secondaryCards,playerB.bestHand.secondaryCards)
+
+                if(secondaryCardComparison == 1){
+                    return playerA
+                }
+                else if(secondaryCardComparison == 2){
+                    return playerB
+                }
+                else{
+                    return "DRAW"
+                }
+            }
+            else{
+                return "DRAW"
+            }
+        }
+    }
+
+    static compareCards(List<Card> cardsA, List<Card> cardsB){
+        int numberOfCards = cardsA.size()
+        int lastCardIndex = numberOfCards -1
+
+        for(lastCardIndex;lastCardIndex>=0;lastCardIndex--){
+            Card cardA = cardsA[lastCardIndex]
+            Card cardB = cardsB[lastCardIndex]
+
+            if(cardA.cardValue.value>cardB.cardValue.value){
+                //println "SEE - " +cardA.cardValue + " is better than " + cardB.cardValue
+                return 1
+            }
+            else if(cardB.cardValue.value > cardA.cardValue.value){
+                //println "SEE - " +cardB.cardValue + " is better than " + cardA.cardValue
+                return 2
+            }
+        }
+
+        return 0
+    }
+
+
 }
